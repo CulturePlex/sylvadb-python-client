4180f8c9a13667ec7e0b5a62907462b8720eeb14
gabmunrio <munozrios22@gmail.com>
2015-09-23 10:59:36 +0200

    Basic structure for the project created.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='',
+      version='0.1',
+      description='',
+      url='https://github.com/CulturePlex/sylvadb-python-client',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=[''],
+      long_description=open("README.txt").read(),
+      install_requires=[],
+      test_suite="",
+      classifiers=[],
+      zip_safe=False)
